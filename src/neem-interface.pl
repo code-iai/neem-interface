a791fb6ac9dad51199a49a369e4e4e3d3cf5de99
HaSu <hawkin@uni-bremen.de>
2024-08-14 17:56:22 +0200

    cramification of the interface. e.g. replace event with action for easier understanding and add documentation strings

--- src/neem-interface.pl
+++ src/neem-interface.pl
@@ -4,12 +4,12 @@
       mem_episode_start(r,r,r,r,r,r,r,+),
       mem_episode_stop(+),
       mem_episode_stop(+,+),
-      mem_event_set_failed(r),
-      mem_event_set_succeeded(r),
-      mem_event_add_diagnosis(r,r),
+      mem_action_set_failed(r),
+      mem_action_set_succeeded(r),
+      mem_action_add_diagnosis(r,r),
       add_subaction_with_task(r,r,r),
-      mem_event_end(r),
-      mem_event_begin(r),
+      mem_action_end(r),
+      mem_action_begin(r),
       belief_perceived_at(r,+,+,r),
       belief_perceived_at(r,r),
       mem_tf_set(r,+,+,+,+),
@@ -26,6 +26,7 @@
 :- use_module(library('db/mongo/client')).
 :- dynamic execution_agent/1.
 
+% do on launch or check how lispcram does it
 mem_clear_memory() :-
     drop_graph(user),
     tf_mem_clear,
@@ -35,6 +36,9 @@ mem_episode_start(Action, EnvOwl, EnvOwlIndiName, EnvUrdf, EnvUrdfPrefix, AgentO
     get_time(StartTime),
     mem_episode_start(Action,  EnvOwl, EnvOwlIndiName, EnvUrdf, EnvUrdfPrefix, AgentOwl, AgentOwlIndiName, AgentUrdf, StartTime).
 
+% this starts everything and describes the root action
+% returns: rootAction
+% start time is rostime
 mem_episode_start(Action, EnvOwl, EnvOwlIndiName, EnvUrdf, EnvUrdfPrefix, AgentOwl, AgentOwlIndiName, AgentUrdf, StartTime) :-
     retractall(execution_agent(_)),
     tf_logger_disable,
@@ -73,22 +77,30 @@ mem_episode_stop(NeemPath, EndTime) :-
     get_time(CurrentTime), atom_concat(NeemPath,'/',X1), atom_concat(X1,CurrentTime,X2), memorize(X2), 
     tf_logger_disable, mem_clear_memory)).
 
-mem_event_set_failed(Action) :- kb_project(action_failed(Action)).
+% set failed or succeeded after the action was completed and after mem_action_end was called
+% TODO add the failure reason using fail roc ont
+mem_action_set_failed(Action) :- kb_project(action_failed(Action)).
 
-mem_event_set_succeeded(Action) :- kb_project(action_succeeded(Action)).
+mem_action_set_succeeded(Action) :- kb_project(action_succeeded(Action)).
 
-mem_event_add_diagnosis(Situation, Diagnosis) :- kb_project(satisfies(Situation, Diagnosis)).
+mem_action_add_diagnosis(Situation, Diagnosis) :- kb_project(satisfies(Situation, Diagnosis)).
 
-add_subaction_with_task(ParentAction,SubAction,TaskType) :-
+
+% ParentAction: the action one has received from mem_episode_start
+% TaskType: soma:'Grasping'
+% returns: SubAction
+add_subaction_with_task(ParentAction,TaskType,SubAction) :-
     execution_agent(Agent),
     kb_project([
         new_iri(SubAction, dul:'Action'), has_type(SubAction,dul:'Action'),
         new_iri(Task, TaskType), has_type(Task,TaskType), executes_task(SubAction,Task),
         holds(ParentAction,dul:hasConstituent,SubAction), % replacement for has_subevent
         is_performed_by(SubAction,Agent)
-    ]),!.
+    ]),
+    mem_action_begin(SubAction),!.
 
-mem_event_end(Event) :- execution_agent(Agent),
+% call this when the Action has finished
+mem_action_end(Event) :- execution_agent(Agent),
     get_time(CurrentTime),
     kb_call([
         triple(Event,dul:'hasTimeInterval',TimeInterval),
@@ -97,13 +109,16 @@ mem_event_end(Event) :- execution_agent(Agent),
     kb_project([holds(TimeInterval, soma:'hasIntervalEnd', CurrentTime),new_iri(Role, soma:'AgentRole'),has_type(Role, soma:'AgentRole')]),
     kb_project([has_role(Agent,Role) during Event, task_role(Task, Role)]),!.
     
-mem_event_begin(Event) :- 
+
+mem_action_begin(Event) :- 
     nonvar(Event),
     get_time(CurrentTime),
     kb_project(is_action(Event)),
     kb_project(occurs(Event) since CurrentTime),!.
 
-mem_event_begin(Event) :- 
+% Event: = dem return value from add_subaction_with_task, so SubAction
+% Does not to be called manually
+mem_action_begin(Event) :- 
     var(Event),
     writeln(Event),
     get_time(CurrentTime),
@@ -111,7 +126,15 @@ mem_event_begin(Event) :-
     writeln(Event),
     kb_project(occurs(Event) since CurrentTime),!.
 
-belief_perceived_at(ObjectType, Mesh, Rotation, Object) :- kb_project([
+
+% Call this after an object has been perceived
+% Mesh is mesh path with rospath
+% ObjectType: soma/dul/suturo:'ObjectType'
+% Positon [1,2,3]
+% Rotation [0,0,0,1]
+% Return: Object
+% TODO add tf frame/link
+belief_perceived_at(ObjectType, Mesh, Position, Rotation, Object) :- kb_project([
     has_type(Object,ObjectType),
     new_iri(ShapeRegion,soma:'MeshShape'), has_type(ShapeRegion, soma:'MeshShape'),
     new_iri(Shape, soma:'Shape'), has_type(Shape, soma:'Shape'),
@@ -120,7 +143,7 @@ belief_perceived_at(ObjectType, Mesh, Rotation, Object) :- kb_project([
     triple(ShapeRegion, soma:'hasFilePath', Mesh),
     new_iri(Origin, soma:'Origin'), has_type(Origin,soma:'Origin'),
     triple(ShapeRegion,'http://knowrob.org/kb/urdf.owl#hasOrigin',Origin),
-    triple(Origin, 'http://www.ease-crc.org/ont/SOMA.owl#hasPositionVector', term([0.0,0.0,0.0])),
+    triple(Origin, 'http://www.ease-crc.org/ont/SOMA.owl#hasPositionVector', term(Position)),
     triple(Origin, 'http://www.ease-crc.org/ont/SOMA.owl#hasOrientationVector', term(Rotation))]).
 
 belief_perceived_at(ObjectType, Object) :- kb_project([has_type(Object,ObjectType)]).
@@ -137,14 +160,17 @@ mem_tf_get(Object, ReferenceFrame, Position, Rotation, Timestamp) :-
     time_scope(=(Timestamp), =(Timestamp), QScope),
     tf_get_pose(Object, [ReferenceFrame, Position, Rotation], QScope, _).
 
-add_participant_with_role(Action, ObjectId, RoleType) :-
+% This is very important since with this we can link objects to their respective actions
+% add a role which is sublac of DUL:Role (check Protege).
+% Most used roles: Item, BeneficiaryRole, Query, Agent, Tool...
+add_participant_with_role(Action, Object, RoleType) :-
     kb_call([executes_task(Action, Task),
             triple(Event,dul:'hasTimeInterval',TimeInterval),
             triple(TimeInterval,soma:'hasIntervalBegin',Start),
             triple(TimeInterval,soma:'hasIntervalEnd',End)]),
-    kb_project([has_participant(Action,ObjectId),
+    kb_project([has_participant(Action,Object),
                 new_iri(Role, RoleType), has_type(Role, RoleType)]),
-    kb_project(has_role(ObjectId,Role) during Action),!.
+    kb_project(has_role(Object,Role) during Action),!.
 
 add_parameter(Task,ParameterType,RegionType) :- kb_project([new_iri(Parameter, ParameterType), has_type(Parameter, ParameterType),
                                                                 new_iri(Region,RegionType),has_type(Region,RegionType),
